32e6d16ddd0986fd087d70129bc4af256e003198
Waqar Haider <waqarastori786@gmail.com>
2019-11-15 11:57:05 -0800

    Python_Assignment

---
+++ Assignment1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Twinkle, twinkle, little star,")
+print("      How I wonder what you are!")
+print("            Up above the world so high,")
+print("            Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("      How I wonder what you are")
+
+
+# In[4]:
+
+
+
+import sys
+print("python version")
+print(sys.version)
+
+
+
+# In[5]:
+
+
+
+import datetime
+now = datetime.datetime.now()
+datetime = now.strftime("%Y-%m-%d %H:%M:%S")
+print(f"date and time : {datetime}")
+
+
+# In[ ]:
+
+
+r = int(input("enter the radius of a circle"))
+a = 3.14*r*r
+print(a)
+
+
+# In[ ]:
+
+
+
+firstname = input("enter your firstname")
+lastname = input("enter your lastname")
+print(lastname +" "+ firstname)
+
+
+# In[ ]:
+
+
+
+a = int(input("enter the fist number")
+b = int(input("enter the 2nd number")
+print(a+b)
+
+
+# In[ ]:
+
+
+
+
